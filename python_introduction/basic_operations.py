25802a7adf31cf615ebe8486d6bee4614f0b2f60
Max <murariax@gmail.com>
2025-05-27 16:37:49 +0300

    modified basic_operations.py

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -3,6 +3,8 @@ number_2 = 5
 
 sum = number_1 + number_2
 sub = number_1 - number_2
+product = number_1 * number_2
 
-print(f"Sum of {number_1} and {number_2} = {sum}")
-print(f"Sub of {number_1} and {number_2} = {sub}")
+print(f"Adiition of {number_1} and {number_2} is {sum}")
+print(f"Subtraction of {number_1} and {number_2} is {sub}")
+print(f"Product of {number_1} and {number_2} is {product}")
