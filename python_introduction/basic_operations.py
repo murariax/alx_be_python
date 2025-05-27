5447a287ab17cbfbbe32a108f60df1c5868b43ea
Max <murariax@gmail.com>
2025-05-27 16:33:28 +0300

    added basic_operations.py

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,8 @@
+number_1 = 10
+number_2 = 5
+
+sum = number_1 + number_2
+sub = number_1 - number_2
+
+print(f"Sum of {number_1} and {number_2} = {sum}")
+print(f"Sub of {number_1} and {number_2} = {sub}")
